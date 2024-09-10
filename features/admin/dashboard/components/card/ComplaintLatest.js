b555fac11d8ca33ae061b267257fd4969c66a243
PORNETWORK-PC\PorNe <poracademic2020@gmail.com>
2024-09-10 11:13:59 +0700

    Update some components in dashboard

--- features/admin/dashboard/components/card/ComplaintLatest.js
+++ features/admin/dashboard/components/card/ComplaintLatest.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Card, Typography } from 'antd'
+import { NotifyCard } from '../notify-card'
 
 const ComplaintLatest = (props) => {
   const { } = props
 
   return (
     <Card>
-      <Typography.Title level={5} className='!m-0'>เรื่องร้องเรียนล่าสุด ภายในวันนี้</Typography.Title>
+      <section className='flex justify-between items-center'>
+        <Typography.Title level={5} className='!m-0'>เรื่องร้องเรียนล่าสุด ภายในวันนี้</Typography.Title>
+        <Typography.Text underline className='!cursor-pointer'>แสดงทั้งหมด</Typography.Text>
+      </section>
+      <section>
+        <NotifyCard />
+      </section>
     </Card>
   )
 }
---
+++ features/admin/dashboard/components/notify-card/index.js
@@ -0,0 +1,1 @@
+export { default as NotifyCard } from './NotifyCard'
