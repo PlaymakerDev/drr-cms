f7a05d783e3cc4af09f8051ce353c73d835440fc
PORNETWORK-PC\PorNe <poracademic2020@gmail.com>
2024-09-17 13:47:14 +0700

    update themeConfig

--- theme/themeConfig.js
+++ theme/themeConfig.js
@@ -29,46 +29,46 @@ const theme = {
       colorText: '#FFFFFF',
     },
     Pagination: {
-			itemActiveBg: '#2b303d',
-			itemBg: 'transparent',
-			colorPrimary: '#ffffff',
-			colorPrimaryHover: '#ffffff',
-			// borderRadius: 50,
-			showSizeChanger: false,
-		},
+      itemActiveBg: '#2b303d',
+      itemBg: 'transparent',
+      colorPrimary: '#ffffff',
+      colorPrimaryHover: '#ffffff',
+      // borderRadius: 50,
+      showSizeChanger: false,
+    },
   },
   token: {
     fontSize: 16,
     colorPrimary: '#0075E9',
     fontFamily: 'IBMPlexSansThai-Regular',
   },
-  components: {
-    Table: {
-    headerBg: '#26344B',
-    headerSplitColor: 'transparent',
-    rowHoverBg: '#232d4e',
-    rowSelectedBg: '#232d4e',
-    rowSelectedHoverBg: '#232d4e',
-    bodySortBg:'transparent',
-    headerSortActiveBg:'#26344B',
-    headerSortHoverBg:'#26344B',
-    fixedHeaderSortActiveBg:'#26344B'
-  },
-  Segmented: {
-    itemColor: '#878787',
-    itemSelectedBg: '#0075E9',
-    itemSelectedColor: '#FFFFFF'
-  },
-  Modal: {
-    titleColor: 'rgba(0, 0, 0)',
-    iconColor: '#FF4D4F',
-    okButtonColor: '#52c41a',
-    okButtonBorderRadius: 0, 
-    okButtonHoverColor: '#389e0d',
-    cancelButtonColor: '#FF4D4F',
-    cancelButtonBorderRadius: 0, 
-  },
-}
+  // components: {
+  //   Table: {
+  //     headerBg: '#26344B',
+  //     headerSplitColor: 'transparent',
+  //     rowHoverBg: '#232d4e',
+  //     rowSelectedBg: '#232d4e',
+  //     rowSelectedHoverBg: '#232d4e',
+  //     bodySortBg: 'transparent',
+  //     headerSortActiveBg: '#26344B',
+  //     headerSortHoverBg: '#26344B',
+  //     fixedHeaderSortActiveBg: '#26344B'
+  //   },
+  //   Segmented: {
+  //     itemColor: '#878787',
+  //     itemSelectedBg: '#0075E9',
+  //     itemSelectedColor: '#FFFFFF'
+  //   },
+  //   Modal: {
+  //     titleColor: 'rgba(0, 0, 0)',
+  //     iconColor: '#FF4D4F',
+  //     okButtonColor: '#52c41a',
+  //     okButtonBorderRadius: 0,
+  //     okButtonHoverColor: '#389e0d',
+  //     cancelButtonColor: '#FF4D4F',
+  //     cancelButtonBorderRadius: 0,
+  //   },
+  // }
 };
 
 
