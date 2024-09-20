b3d01a3d4858251a879d31cc110d76472344d7b0
PORNETWORK-PC\PorNe <poracademic2020@gmail.com>
2024-09-20 16:37:10 +0700

    update redux-store

--- store/features/index.js
+++ store/features/index.js
@@ -1,20 +1,16 @@
 import user from './userSlice'
 import tasksRunning from './tasksRunningSlice'
 import example from './exampleSlice'
-import vehicleWeight from './vehicleWeightSlice'
 import dashboard from './dashboardSlice'
 import master from './masterSlice'
-import information from './informationSlice'
 // {{import}}
 
 const rootReducer = {
   user,
   tasksRunning,
 	example,
-	vehicleWeight,
 	dashboard,
 	master,
-	information,
 // {{export}}
 }
 
