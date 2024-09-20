cbd1ad9bd7465ed6c94c15db88e76d934831d469
PORNETWORK-PC\PorNe <poracademic2020@gmail.com>
2024-09-20 16:54:18 +0700

    update local reducer

--- store/features/dashboardSlice.js
+++ store/features/dashboardSlice.js
@@ -4,6 +4,33 @@ import { HYDRATE } from 'next-redux-wrapper';
 const name = "dashboard"
 
 export const initialState = {
+  top3_complain: {
+    search: {
+      dateSearch: '',
+    },
+    data: {
+      graph: {
+        series: [],
+        labels: []
+      },
+      data: []
+    }
+  },
+  top3_progress: {
+    search: {
+      dateSearch: '',
+    },
+    data: {
+      series: [],
+      labels: []
+    },
+  },
+  most_popular: {
+    search: {
+      dateSearch: '',
+    },
+    data: []
+  }
 }
 
 export const slice = createSlice({
